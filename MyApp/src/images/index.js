8d4e8a7f99636ee1bf25b35f7d43f4c6fdaa00e8
Rajveer Singh <rajveersingh.charan@gmail.com>
2020-11-05 18:42:32 +0530

    - src folder

---
+++ MyApp/src/images/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+export const AppLogo = require('./keycloak-logo.png');
+export const logo = require('./logo.png');
+export const warning = require('./warning_icon.png');
+export const barcode = require('./barcode.png');
+export const search = require('./search.png');
+export const cancel = require('./cancel.png');
+export const cancel_icon = require('./cancel_icon.png');
+export const check = require('./check.png');
+
